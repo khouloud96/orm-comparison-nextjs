e72b21dfc40c03400941e26646f14cfa13d41e75
Khouloud Ben Salem <khouloud.bensalem@ensi-uma.tn>
2025-01-28 15:32:31 +0100

    poc typeorm

--- src/lib/db-init.ts
+++ src/lib/db-init.ts
@@ -1,7 +1,7 @@
 /*
 Initialiser la connexion à la base de données si elle n'est pas déjà établie.
 */
-import { AppDataSource } from "./typeorm";
+import { AppDataSource } from "./data-source";
 
 export const initializeDB = async () => {
   if (!AppDataSource.isInitialized) {
